c6f978d944a2d4861ea99911e2f61dadb56b0acb
mathewp88 <49433106+mathewp88@users.noreply.github.com>
2022-12-03 18:17:36 +0530

    General Update

--- 2022/1/code.rs
+++
@@ -1,7 +0,0 @@
-// Advent of code Year 2022 Day 1 solution
-// Author = Mathai Mathew
-// Date = December 2022
-
-fn main() {
-    println!("Hello, world!");
-}
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Year 2022
 
-Get Familiar with Rust and its syntax
+Intro
